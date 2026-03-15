c9aa27f1d260ef694f853a8bfe1f6370d4911d17
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2026-03-16 01:47:15 +0530

    cosmetic changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from "path"
 import express from "express";
 import dotenv from 'dotenv'
+dotenv.config()
 import authRoutes from "./routes/auth.routes.js"
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.routes.js"
@@ -11,7 +12,6 @@ import { app,server } from "./socket/socket.js";
 const PORT=process.env.PORT||5000
 const __dirname=path.resolve()
 
-dotenv.config()
 
 
 app.use(express.json())
